9af959f02c1df2cda1dea6cbbfcf85c047edaeda
AbanoubEmadM <emadbebo729@gmail.com>
2026-05-19 19:54:45 +0300

    Add Role for Employees(Admin/Cashier)

--- database/seeders/EmployeeSeeder.php
+++ database/seeders/EmployeeSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Employee;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class EmployeeSeeder extends Seeder
 {
@@ -11,5 +12,13 @@ class EmployeeSeeder extends Seeder
     {
 
         Employee::factory(3)->create();
+        Employee::factory()->create(
+            [
+                'name' => 'admin',
+                'email' => 'admin@admin.com',
+                'role' => 'admin',
+                'password' => Hash::make('password'),
+            ]
+        );
     }
 }
