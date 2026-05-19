b1f3a74f18bcc985e6bc4aa80202e06e05ebb161
AbanoubEmadM <emadbebo729@gmail.com>
2026-05-19 15:00:23 +0300

    Remove User model, related factories, migrations, and seeders to streamline the application structure.

--- database/factories/EmployeeFactory.php
+++ database/factories/EmployeeFactory.php
@@ -2,22 +2,44 @@
 
 namespace Database\Factories;
 
-use App\Models\Employee;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 /**
- * @extends Factory<Employee>
+ * @extends Factory<User>
  */
-class EmployeeFactory extends Factory
+class UserFactory extends Factory
 {
-    protected $model = Employee::class;
+    /**
+     * The current password being used by the factory.
+     */
+    protected static ?string $password;
 
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
     public function definition(): array
     {
         return [
-            'username' => fake()->unique()->userName(),
-            'password' => Hash::make('password'),
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'email_verified_at' => now(),
+            'password' => static::$password ??= Hash::make('password'),
+            'remember_token' => Str::random(10),
         ];
+    }
+
+    /**
+     * Indicate that the model's email address should be unverified.
+     */
+    public function unverified(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'email_verified_at' => null,
+        ]);
     }
 }
