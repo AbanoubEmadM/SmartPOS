ebb603c3f9b285bed2b676dba25b64ab005490ce
AbanoubEmadM <emadbebo729@gmail.com>
2026-05-22 16:16:24 +0300

    edit policies

--- app/Policies/CategoryPolicy.php
+++ app/Policies/CategoryPolicy.php
@@ -4,7 +4,6 @@ namespace App\Policies;
 
 use App\Models\Category;
 use App\Models\Employee;
-use App\Models\User;
 use Illuminate\Auth\Access\Response;
 
 class CategoryPolicy
@@ -20,10 +19,6 @@ class CategoryPolicy
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, Category $category): bool
-    {
-        return false;
-    }
 
     /**
      * Determine whether the user can create models.
