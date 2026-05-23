381de526aaab4d240ffc7b9e8c8995784ee354a1
AbanoubEmadM <emadbebo729@gmail.com>
2026-05-24 00:50:07 +0300

    Adjust Widgets Position

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Pages;
 
+use App\App\Filament\Widgets\StatsOverview;
 use Filament\Pages\Page;
 use Filament\Widgets\AccountWidget;
 use Illuminate\Support\Facades\Auth;
@@ -12,11 +13,7 @@ class Dashboard extends Page
     protected function getHeaderWidgets(): array
     {
         return [
-            \App\Filament\Widgets\TodayRevenueWidget::class,
-            \App\Filament\Widgets\TodayOrdersWidget::class,
-            \App\Filament\Widgets\LowStockAlertsWidget::class,
-            \App\Filament\Widgets\OrderSalesChartWidget::class,
-            \App\Filament\Widgets\TopSellingProductsWidget::class,
+            StatsOverview::class
         ];
     }
 }
